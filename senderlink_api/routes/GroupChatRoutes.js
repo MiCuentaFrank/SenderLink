fa1fb0c8fdd102c8b1aa8994087d2ae4a3092142
mono <tobio@mio.com>
2026-04-25 21:56:51 +0200

    senderlink1.0 reparacion de la cuenta de firebase2

--- senderlink_api/routes/GroupChatRoutes.js
+++ senderlink_api/routes/GroupChatRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {
   sendMessage,
   getMessages
-} = require("../controllers/groupChatController");
+} = require("../controllers/GroupChatController");
 
 /**
  * ===========================================
