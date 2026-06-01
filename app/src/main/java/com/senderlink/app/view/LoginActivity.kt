6efb1128d85ca27179183345cab1f4fa11cac00a
mono <tobio@mio.com>
2026-06-01 13:53:19 +0200

    actualizar perfil1.0

--- app/src/main/java/com/senderlink/app/view/LoginActivity.kt
+++ app/src/main/java/com/senderlink/app/view/LoginActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.google.firebase.auth.FirebaseAuth
 import com.senderlink.app.databinding.ActivityLoginBinding
 
@@ -13,9 +16,16 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Ajustar padding inferior del card para no solaparse con la barra de navegación
+        val cardBasePaddingBottom = binding.loginFormCard.paddingBottom
+        ViewCompat.setOnApplyWindowInsetsListener(binding.loginFormCard) { view, insets ->
+            val navBarHeight = insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom
+            view.updatePadding(bottom = cardBasePaddingBottom + navBarHeight)
+            insets
+        }
 
         auth = FirebaseAuth.getInstance()
 
